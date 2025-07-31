02f1307b34a5baf02c6b32e3a3952fa120664394
satyendra813 <satyendra9173@gmail.com>
2025-07-31 21:31:56 +0530

    changing port

--- server.js
+++ server.js
@@ -68,5 +68,5 @@ io.on("connection", (socket) => {
 	});
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(3000, () => console.log(`Listening on port ${PORT}`));
+const PORT = process.env.PORT;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
